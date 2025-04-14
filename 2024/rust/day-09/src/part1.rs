7d42f0cca41feac174908e60eba23a8aef4f5794
Alexander Lee <alexblee808@gmail.com>
2025-04-13 18:14:00 -0700

    Partially completed day 9. Part 2 is incomplete

--- 2024/rust/day-09/src/part1.rs
+++ 2024/rust/day-09/src/part1.rs
@@ -6,133 +6,56 @@ pub fn process(input: &str) -> miette::Result<String> {
         .map(|c| c.to_digit(10).unwrap())
         .collect();
 
-
-
-    let mut value_index: usize = 0;
-    let mut memory_index: u32 = 0;
-    let mut move_memory_index: u32 =
-    values.iter().sum::<u32>() - 1;
-    let mut move_file_id: u32 = values.len() as u32 / 2;
-
-    let mut checksum = 0;
-    while memory_index < move_memory_index {
-        match value_index % 2 { 
-            0 => {
-                // Normal file, add away
-                let file_id = (value_index / 2) as u32;
-                match values[value_index] {
-                    0 => value_index += 1,
-                    _ => checksum += file_id * memory_index
-                        
-                    
-                }
-
-                
-
-            }
-            _ => 
-        };
-
-        values[value_index] = -1;
-
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    let mut index: u32 = 0;
-    let mut file_id: u32 = 0;
-
-    let mut shift_index: u32 =
+    // Disk Space Pointers
+    let mut position_front: u32 = 0;
+    let mut position_end: u32 =
         values.iter().sum::<u32>() - 1;
-    let mut shift_file_id: u32 = values.len() as u32 / 2;
+    // ID Number
+    let mut id_number_front: u32 = 0;
+    let mut id_number_end: u32 =
+        (values.len() / 2).try_into().unwrap();
+    // Disk Map Index
+    let mut index_front: usize = 0;
+    let mut index_end: usize = values.len() - 1;
 
-    dbg!(&values, shift_file_id, shift_index);
-
-    let mut checksum = 0;
-    let mut is_file: bool = true;
-    while index < shift_index {
-        match is_file {
-            true => {
-                checksum += 
-                index += 1;
-            }
-            false => {}
+    let mut checksum: u64 = 0;
+    while position_end >= position_front {
+        // Increment the disk map entries being processed
+        if values[index_front] == 0 {
+            index_front += 1;
+            id_number_front = index_front as u32 / 2;
+            continue;
         }
+        if values[index_end] == 0 {
+            index_end -= 1;
+            id_number_end = index_end as u32 / 2;
+            continue;
+        }
+
+        // Handle case when a file is being allocated from
+        // the left
+        if index_front % 2 == 0 {
+            checksum +=
+                (id_number_front * position_front) as u64;
+            values[index_front] -= 1;
+            position_front += 1;
+            continue;
+        }
+
+        // Handle case when a file is being allocated from
+        // the right
+        // Skip over the free space
+        if index_end % 2 == 1 {
+            position_end -= values[index_end];
+            values[index_end] = 0;
+            continue;
+        }
+        checksum += (id_number_end * position_front) as u64;
+        values[index_end] -= 1;
+        values[index_front] -= 1;
+        position_front += 1;
+        position_end -= 1;
     }
-
-
-    
-
-
-
-    
-
-
-
-
-    todo!();
-    let mut spaces: Vec<i32> = Vec::with_capacity(
-        values.iter().sum::<u32>() as usize,
-    );
-
-    values.iter().enumerate().for_each(|(i, count)| {
-        let value: i32 =
-            if 0 == i % 2 { (i / 2) as i32 } else { -1 };
-        for _ in 0..*count {
-            spaces.push(value);
-        }
-    });
-    dbg!(&spaces);
-
-    let mut checksum = 0;
-    let mut index: i32 = 0;
-    let mut shift_index: i32 = spaces.len() as i32;
-
-    while index < shift_index {
-        match spaces[index as usize] {
-            -1 => {
-                shift_index += -1;
-                match spaces[shift_index as usize] {
-                    -1 => continue,
-                    _ => {
-                        checksum += spaces
-                            [shift_index as usize]
-                            * index;
-                        index += 1;
-                    }
-                }
-            }
-            _ => {
-                checksum += spaces[index as usize] * index;
-                index += 1
-            }
-        }
-        dbg!(checksum);
-    }
-
     Ok(checksum.to_string())
 }
 
--- 2024/rust/day-09/src/part2.rs
+++ 2024/rust/day-09/src/part2.rs
@@ -1,6 +1,92 @@
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
-    todo!("day 01 - part 2");
+    let mut values: Vec<u32> = input
+        .chars()
+        .filter(|c| c.is_numeric())
+        .map(|c| c.to_digit(10).unwrap())
+        .collect();
+
+    // Disk Space Pointers
+    let mut position_front: u32 = 0;
+    let mut position_end: u32 =
+        values.iter().sum::<u32>() - 1;
+    // ID Number
+    let mut id_number_front: u32 = 0;
+    let mut id_number_end: u32 =
+        (values.len() / 2).try_into().unwrap();
+    // Disk Map Index
+    let mut index_front: usize = 0;
+    let mut index_end: usize = values.len() - 1;
+
+    let mut checksum: u64 = 0;
+    while position_end >= position_front
+        && index_front < values.len()
+    {
+        // Increment the disk map entries being processed
+        if values[index_front] == 0 {
+            index_front += 1;
+            id_number_front = index_front as u32 / 2;
+            continue;
+        }
+        if values[index_end] == 0 {
+            index_end -= 1;
+            id_number_end = index_end as u32 / 2;
+            continue;
+        }
+
+        // Handle case when a file is being allocated from
+        // the left
+        if is_file(index_front) {
+            println!("{}", id_number_front);
+            checksum +=
+                (id_number_front * position_front) as u64;
+            values[index_front] -= 1;
+            position_front += 1;
+            continue;
+        }
+
+        // Handle case when a file is being allocated from
+        // the right
+        // Skip over the free space
+        if !!!is_file(index_end) {
+            position_end -= values[index_end];
+            values[index_end] = 0;
+            continue;
+        }
+
+        // Loop over the file IDs in decreasing order
+        let mut inner_index_end: usize = index_end;
+        while index_front < inner_index_end {
+            if index_front >= inner_index_end {
+                values[index_front] = 0;
+                break;
+            } else if is_file(inner_index_end) && values[index_front]
+                >= values[inner_index_end]
+                && values[inner_index_end] > 0
+            {
+                let inner_id_number_end =
+                    index_end as u32 / 2;
+                for _ in 0..values[inner_index_end] {
+                    checksum += (inner_id_number_end
+                        * position_front)
+                        as u64;
+                    position_front += 1;
+                    position_end -= 1;
+                    println!("{}", inner_id_number_end);
+                }
+                values[index_front] -=
+                    values[inner_index_end];
+                values[inner_index_end] = 0;
+            }
+            inner_index_end -= 1;
+        }
+        
+
+    Ok(checksum.to_string())
+}
+
+fn is_file(disk_index: usize) -> bool {
+    disk_index % 2 == 0
 }
 
 #[cfg(test)]
@@ -10,7 +96,7 @@ mod tests {
     #[test]
     fn test_process() -> miette::Result<()> {
         let input = "2333133121414131402";
-        assert_eq!("1928", process(input)?);
+        assert_eq!("2858", process(input)?);
         Ok(())
     }
 }
