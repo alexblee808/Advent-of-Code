651daf0dd3909baff963b963609b917e8dd765e6
alexblee808 <alexblee808@gmail.com>
2025-05-10 21:38:04 +0300

    Nominally completed day 9 part 2. Need test input to confirm.

--- 2024/rust/day-09/src/part2.rs
+++ 2024/rust/day-09/src/part2.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String> {
     let mut values: Vec<u32> = input
@@ -12,111 +14,113 @@ pub fn process(input: &str) -> miette::Result<String> {
         values.iter().sum::<u32>() - 1;
     // ID Number
     let mut id_number_front: u32 = 0;
-    let mut id_number_end: u32 =
-        (values.len() / 2).try_into().unwrap();
     // Disk Map Index
     let mut index_front: usize = 0;
     let mut index_end: usize = values.len() - 1;
 
     let mut checksum: u64 = 0;
+    // Use Vec of same size as values initialized to
+    // zeroes
+    let mut allocated: HashMap<usize, u32> = HashMap::new();
     while position_end >= position_front
         && index_front < values.len()
     {
-        // Increment the disk map entries being processed
-        if values[index_front] == 0 {
+        // Handle case when a file is being allocated from
+        // the left
+        if is_file(index_front) {
+            // Check if the file has been added from the
+            // right
+            if allocated.contains_key(&index_front) {
+                position_front += allocated[&index_front];
+            } else {
+                let length: u32 = values[index_front];
+                checksum += (id_number_front
+                    * (position_front * length
+                        + (0..length).sum::<u32>()))
+                    as u64;
+                // // DELETE ME
+                // for _ in 0..length {
+                //     dbg!("{}", &id_number_front);
+                // }
+                // // DELETE ME
+                position_front += length;
+            }
+
+            values[index_front] = 0;
             index_front += 1;
             id_number_front = index_front as u32 / 2;
             continue;
         }
-        if values[index_end] == 0 {
-            index_end -= 1;
-            id_number_end = index_end as u32 / 2;
-            continue;
-        }
 
-        // Remove free space from the right
-        if !!!is_file(index_end) {
-            position_end -= values[index_end];
-            values[index_end] = 0;
+        // TEST WITH AND WITHOUT
+        if allocated.contains_key(&index_end) {
+            position_end -= allocated[&index_end]
+                + values[index_end - 1];
+            index_end -= 2;
         }
+        // TEST WITH AND WITHOUT
 
-        // Allocate the file from the right
-        let mut inner_index_front: usize = index_front;
-        while inner_index_front < index_end {
-            if is_file(inner_index_front)
-                || values[inner_index_front]
-                    < values[index_end]
-            {
-                inner_index_front += 1;
+        // Attempt to allocate file from right
+        let mut free_space: u32 = values[index_front];
+        let mut index_end_inner: usize = index_end;
+        while free_space > 0
+            && index_front < index_end_inner
+        {
+            let length_end: u32 = values[index_end_inner];
+            if length_end > free_space || length_end == 0 {
+                index_end_inner -= 2;
+                continue;
             }
-            for _ in 0..values[index_end] {
-                checksum +=
-                    (position_front * id_number_end) as u64;
-                position_front += 1;
-            }
-            values[inner_index_front] -= values[index_end];
-            values[index_end] = 0;
-            break;
+            let id_number_end_inner: u32 =
+                index_end_inner as u32 / 2;
+            checksum += (id_number_end_inner
+                * (position_front * length_end
+                    + (0..length_end).sum::<u32>()))
+                as u64;
+            // // DELETE ME
+            // for _ in 0..length_end {
+            //     println!("{}", &id_number_end_inner);
+            // }
+            // // DELETE ME
+
+            allocated.insert(index_end_inner, length_end);
+            values[index_end_inner] = 0;
+            position_front += length_end;
+            free_space -= length_end;
+            index_end_inner -= 2;
         }
-
-        // Allocate the file inplace because it cannot be
-        // moved
-        if values[index_end] != 0 {
-            println!("Adding file in place");
+        if free_space > 0 {
+            position_front += free_space;
         }
+        values[index_front] = 0;
+        index_front += 1;
+        id_number_front = index_front as u32 / 2;
 
-        // // Handle case when a file is being
-        // allocated from // the left
-        // if is_file(index_front) {
-        //     println!("{}", id_number_front);
-        //     checksum +=
-        //         (id_number_front *
-        // position_front) as u64;
-        //     values[index_front] -= 1;
-        //     position_front += 1;
-        //     continue;
-        // }
-
-        // // Handle case when a file is being
-        // allocated from // the right
-        // // Skip over the free space
-        // if !!!is_file(index_end) {
-        //     position_end -= values[index_end];
-        //     values[index_end] = 0;
-        //     continue;
-        // }
-
-        // // Loop over the file IDs in decreasing
-        // order
-        // let mut inner_index_front: usize =
-        // index_front; while index_front
-        // < inner_index_end {
-        //     if index_front >= inner_index_end {
-        //         values[index_front] = 0;
-        //         break;
-        //     } else if is_file(inner_index_end)
-        // && values[index_front]
-        //         >= values[inner_index_end]
-        //         && values[inner_index_end] > 0
-        //     {
-        //         let inner_id_number_end =
-        //             index_end as u32 / 2;
-        //         for _ in
-        // 0..values[inner_index_end] {
-        //             checksum +=
-        // (inner_id_number_end
-        //                 * position_front)
-        //                 as u64;
-        //             position_front += 1;
-        //             position_end -= 1;
-        //             println!("{}",
-        // inner_id_number_end);         }
-        //         values[index_front] -=
-        //             values[inner_index_end];
-        //         values[inner_index_end] = 0;
+        //     // Remove free space from the right
+        //     if !!!is_file(index_end) {
+        //         position_end -=
+        // values[index_end];
+        //         values[index_end] = 0;
         //     }
-        //     inner_index_end -= 1;
-        // }
+
+        //     if is_file(index_front) {
+        //         let length: u32 =
+        // values[index_front];
+        //             checksum +=
+        //             (id_number_front * length *
+        // position_front)                 as u64
+        //                 + (0..length).
+        //                   sum::<u32>() as u64;
+
+        //         position_front += length;
+        //         index_front += 1;
+        //         id_number_front = index_front
+        // as u32 /    2;
+
+        //      }
+
+        //     (index_front+1..index_end)
+        //     for index_end_inner in
     }
 
     Ok(checksum.to_string())
